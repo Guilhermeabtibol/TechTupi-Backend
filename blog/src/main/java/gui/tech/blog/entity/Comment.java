11927e645600d5047489b02ccde5ce9e5ffb2a6d
guilhermeabtibol <guiabtibol@gmail.com>
2025-02-26 07:55:40 -0400

    criando modelo comment

---
+++ blog/src/main/java/gui/tech/blog/entity/Comment.java
@@ -0,0 +1,40 @@
+package gui.tech.blog.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false) // Define a coluna que referencia o Post
+    private Post post; // Agora é um único Post
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post; // Método para definir o post associado
+    }
+}
